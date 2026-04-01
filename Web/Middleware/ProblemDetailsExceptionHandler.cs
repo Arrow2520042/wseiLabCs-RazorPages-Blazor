a5d9e804bf9ec86e21c14f3f8723f9bd2270788f
Piotr Wydymus <piotr.wydymus@microsoft.wsei.edu.pl>
2026-04-01 10:43:24 +0200

    lab5 done

--- Web/Middleware/ProblemDetailsExceptionHandler.cs
+++ Web/Middleware/ProblemDetailsExceptionHandler.cs
@@ -2,34 +2,43 @@ using ApplicationCore.Exceptions;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace BackendLab01.Middleware;
-
-public class ProblemDetailsExceptionHandler(
-    ProblemDetailsFactory factory,
-    ILogger<ProblemDetailsExceptionHandler> logger) : IExceptionHandler
+namespace BackendLab01.Middleware
 {
-    public async ValueTask<bool> TryHandleAsync(
-        HttpContext context,
-        Exception exception,
-        CancellationToken cancellationToken)
+    public class ProblemDetailsExceptionHandler : IExceptionHandler
     {
-        if (exception is ContactNotFoundException or NoteNotFoundException)
+        private readonly ProblemDetailsFactory _factory;
+        private readonly ILogger<ProblemDetailsExceptionHandler> _logger;
+
+        public ProblemDetailsExceptionHandler(ProblemDetailsFactory factory, ILogger<ProblemDetailsExceptionHandler> logger)
         {
-            logger.LogInformation("Exception '{ExceptionMessage}' handled!", exception.Message);
-
-            var problem = factory.CreateProblemDetails(
-                context,
-                StatusCodes.Status400BadRequest,
-                "Contact service error!",
-                "Service error",
-                detail: exception.Message);
-
-            context.Response.StatusCode = StatusCodes.Status400BadRequest;
-            await context.Response.WriteAsJsonAsync(problem, cancellationToken);
-            return true;
+            _factory = factory;
+            _logger = logger;
         }
 
-        return false;
+        public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
+        {
+            if (exception is ContactNotFoundException || exception is NoteNotFoundException)
+            {
+                _logger.LogInformation("Exception '{ExceptionMessage}' handled!", exception.Message);
+
+                var problem = _factory.CreateProblemDetails(
+                    context,
+                    StatusCodes.Status400BadRequest,
+                    "Contact service error!",
+                    "Service error",
+                    detail: exception.Message);
+
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                await context.Response.WriteAsJsonAsync(problem, cancellationToken);
+                return true;
+            }
+
+            return false;
+        }
     }
 }
