f52fee8d9142cba38048956b2f52c237eceb6566
Arrow25 <qwertzarrow25bs@gmail.com>
2026-04-15 15:29:56 +0200

    bugfixes

--- ApplicationCore/Entities/CrmRole.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace ApplicationCore.Entities
-{
-    public class CrmRole : IdentityRole
-    {
-        public string Description { get; set; } = string.Empty;
-    }
-}
